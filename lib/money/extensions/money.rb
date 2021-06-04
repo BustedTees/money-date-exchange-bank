34a920f09aaaf20b44a786930a9b136845aaf9b1
justindhall <hall.justin.d@gmail.com>
2021-06-04 12:37:08 -0400

    removes puts on monkey patch

--- lib/money/extensions/money.rb
+++ lib/money/extensions/money.rb
@@ -6,7 +6,6 @@ module Extensions
   # MonkeyPatch of the Money class to allow for exchange_to with date or rate.
   module Money
     def self.included(base)
-      puts 'Overriding Money#exchange_to, original method available as Money#orig_exchange_to.'
       base.class_eval do
         alias_method :orig_exchange_to, :exchange_to
         def exchange_to(other_currency, date: nil, rate: nil, &rounding_method)
